5bfb363b6e72054c0b2141610ed58f16114164e0
Dreamwalker <aristojeff@gmail.com>
2022-03-23 20:06:58 +0900

    Create mail_inbox_home_page.dart

---
+++ lib/ep1101_mail_inbox_app/src/mail_inbox_home_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class MainInboxHomePage extends StatefulWidget {
+  const MainInboxHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MainInboxHomePage> createState() => _MainInboxHomePageState();
+}
+
+class _MainInboxHomePageState extends State<MainInboxHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.menu),
+                    ),
+                    const Expanded(
+                        child: Center(
+                      child: Text(
+                        "Inbox (122)",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.search),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Divider(
+              height: 0,
+              color: Colors.grey[300],
+            ),
+            Expanded(
+              child: ListView(
+                children: [],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
