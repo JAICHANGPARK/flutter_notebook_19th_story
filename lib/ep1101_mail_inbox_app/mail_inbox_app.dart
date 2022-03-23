b4befcfe548517992a721c55fde89ff2ae7ba56a
Dreamwalker <aristojeff@gmail.com>
2022-03-23 19:59:55 +0900

    Create mail_inbox_app.dart

---
+++ lib/ep1101_mail_inbox_app/mail_inbox_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MailInboxApp extends StatelessWidget {
+  const MailInboxApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
