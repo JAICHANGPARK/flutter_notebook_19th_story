f2f77e5c873076895077f32ac6860cda2153bfeb
Dreamwalker <aristojeff@gmail.com>
2022-03-23 19:57:02 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_19th_story/ep1101_mail_inbox_app/mail_inbox_app.dart';
 
 
 main() {
-  runApp(app);
+  runApp(MailInboxApp());
 }
