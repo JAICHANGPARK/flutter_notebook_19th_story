f589c02aa821fc9a4ebb9f361b77a39cdd3252bc
Dreamwalker <aristojeff@gmail.com>
2022-03-25 20:12:32 +0900

    Update new_message_page.dart

--- lib/ep1101_mail_inbox_app/src/ui/new_message_page.dart
+++ lib/ep1101_mail_inbox_app/src/ui/new_message_page.dart
@@ -1,2 +1,128 @@
 import 'package:flutter/material.dart';
 
+class NewMessagePage extends StatefulWidget {
+  const NewMessagePage({Key? key}) : super(key: key);
+
+  @override
+  State<NewMessagePage> createState() => _NewMessagePageState();
+}
+
+class _NewMessagePageState extends State<NewMessagePage> {
+  TextEditingController _subjectTextEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              height: 64,
+              color: Colors.white,
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: const Icon(
+                      Icons.navigate_before,
+                    ),
+                  ),
+                  const Expanded(
+                    child: Center(
+                        child: Padding(
+                      padding: EdgeInsets.only(left: 24),
+                      child: Text(
+                        "New message",
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                    )),
+                  ),
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.send)),
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert))
+                ],
+              ),
+            ),
+            const Divider(
+              height: 0,
+              color: Colors.grey,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              child: Row(
+                children: [
+                  const Text("To:"),
+                  const SizedBox(
+                    width: 12,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.blue[50],
+                        border: Border.all(
+                          color: Colors.grey[300]!,
+                        ),
+                        borderRadius: BorderRadius.circular(4)),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 2,
+                      vertical: 2,
+                    ),
+                    child: Row(
+                      children: const [
+                        Text(
+                          "sample@gmail.com",
+                          style: TextStyle(
+                            fontSize: 12,
+                          ),
+                        ),
+                        Icon(
+                          Icons.clear,
+                          size: 16,
+                        )
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                  const Icon(
+                    Icons.keyboard_arrow_down,
+                  )
+                ],
+              ),
+            ),
+            const Divider(
+              height: 0,
+              color: Colors.grey,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Row(
+                children: [
+                  const Text("Subject:"),
+                  const SizedBox(
+                    width: 12,
+                  ),
+                  Expanded(
+                    child: SizedBox(
+                      height: 32,
+                      child: TextField(
+                        controller: _subjectTextEditingController,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Divider(
+              height: 0,
+              color: Colors.grey,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
