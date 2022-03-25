6051fcf6b1c55e9c30419e43a3b8a2c52b60c5b2
Dreamwalker <aristojeff@gmail.com>
2022-03-25 19:55:07 +0900

    Update mail_inbox_home_page.dart

--- lib/ep1101_mail_inbox_app/src/ui/mail_inbox_home_page.dart
+++ lib/ep1101_mail_inbox_app/src/ui/mail_inbox_home_page.dart
@@ -63,90 +63,103 @@ class _MainInboxHomePageState extends State<MainInboxHomePage> {
                       padding: const EdgeInsets.all(8.0),
                       child: ListView.separated(
                         itemBuilder: (context, index) {
-                          return Container(
-                            height: 86,
-                            padding: const EdgeInsets.all(8),
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text(
-                                            items[index].title ?? "",
+                          Key _key = Key("$index");
+                          return Dismissible(
+                            onDismissed: (d) {
+                              print(d);
+                            },
+                            background: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.red,
+                              ),
+                            ),
+                            direction: DismissDirection.endToStart,
+                            key: _key,
+                            child: Container(
+                              height: 86,
+                              padding: const EdgeInsets.all(8),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(
+                                              items[index].title ?? "",
+                                              style: const TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 12,
+                                              ),
+                                            ),
+                                            (items[index].isNew ?? false)
+                                                ? Container(
+                                                    decoration: BoxDecoration(
+                                                      color: Colors.red,
+                                                      borderRadius: BorderRadius.circular(6),
+                                                    ),
+                                                    padding: const EdgeInsets.symmetric(
+                                                      horizontal: 4,
+                                                      vertical: 2,
+                                                    ),
+                                                    child: const Center(
+                                                      child: Text(
+                                                        "New",
+                                                        style: TextStyle(
+                                                          color: Colors.white,
+                                                          fontSize: 12,
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  )
+                                                : Container(),
+                                          ],
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(vertical: 12),
+                                          child: Text(
+                                            items[index].subtitle ?? "",
                                             style: const TextStyle(
                                               fontWeight: FontWeight.bold,
                                               fontSize: 12,
                                             ),
                                           ),
-                                          (items[index].isNew ?? false)
-                                              ? Container(
-                                                  decoration: BoxDecoration(
-                                                    color: Colors.red,
-                                                    borderRadius: BorderRadius.circular(6),
-                                                  ),
-                                                  padding: const EdgeInsets.symmetric(
-                                                    horizontal: 4,
-                                                    vertical: 2,
-                                                  ),
-                                                  child: const Center(
-                                                    child: Text(
-                                                      "New",
-                                                      style: TextStyle(
-                                                        color: Colors.white,
-                                                        fontSize: 12,
-                                                      ),
-                                                    ),
-                                                  ),
-                                                )
-                                              : Container(),
-                                        ],
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.symmetric(vertical: 12),
-                                        child: Text(
-                                          items[index].subtitle ?? "",
+                                        ),
+                                        Text(
+                                          items[index].overlayText ?? "",
                                           style: const TextStyle(
                                             fontWeight: FontWeight.bold,
                                             fontSize: 12,
+                                            color: Colors.grey,
+                                            overflow: TextOverflow.ellipsis,
                                           ),
                                         ),
-                                      ),
-                                      Text(
-                                        items[index].overlayText ?? "",
-                                        style: const TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 12,
-                                          color: Colors.grey,
-                                          overflow: TextOverflow.ellipsis,
-                                        ),
-                                      ),
-                                    ],
+                                      ],
+                                    ),
                                   ),
-                                ),
-                                const SizedBox(
-                                  width: 8,
-                                ),
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  mainAxisSize: MainAxisSize.max,
-                                  children: [
-                                    Text(items[index].time ?? "--:--"),
-                                    (items[index].isFavorite ?? false)
-                                        ? const Icon(
-                                            Icons.star,
-                                            size: 18,
-                                          )
-                                        : const Icon(
-                                            Icons.star_border,
-                                            size: 18,
-                                          )
-                                  ],
-                                )
-                              ],
+                                  const SizedBox(
+                                    width: 8,
+                                  ),
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    mainAxisSize: MainAxisSize.max,
+                                    children: [
+                                      Text(items[index].time ?? "--:--"),
+                                      (items[index].isFavorite ?? false)
+                                          ? const Icon(
+                                              Icons.star,
+                                              size: 18,
+                                            )
+                                          : const Icon(
+                                              Icons.star_border,
+                                              size: 18,
+                                            )
+                                    ],
+                                  )
+                                ],
+                              ),
                             ),
                           );
                         },
