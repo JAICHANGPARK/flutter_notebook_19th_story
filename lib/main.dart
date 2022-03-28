1b6df4c03a7b91efdaf5ed922c641a9b968f7b5c
Dreamwalker <aristojeff@gmail.com>
2022-03-28 18:41:15 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_19th_story/ep1101_mail_inbox_app/mail_inbox_app.dart';
+import 'package:flutter_notebook_19th_story/ep1105_fitness_workout_app/ep1105_fitness_workout_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 main() {
   runApp(
     const ProviderScope(
-      child: MailInboxApp(),
+      child: FitnessWorkoutApp(),
     ),
   );
 }
