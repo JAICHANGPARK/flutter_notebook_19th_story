5d0d5ae82f96a3a23d0fe1a709ebe39a1fad6277
Dreamwalker <aristojeff@gmail.com>
2022-03-28 18:38:46 +0900

    Update fitness_workout_home_page.dart

---
+++ lib/ep1105_fitness_workout_app/src/ui/fitness_workout_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FitnessWorkoutHomePage extends StatefulWidget {
+  const FitnessWorkoutHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<FitnessWorkoutHomePage> createState() => _FitnessWorkoutHomePageState();
+}
+
+class _FitnessWorkoutHomePageState extends State<FitnessWorkoutHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
