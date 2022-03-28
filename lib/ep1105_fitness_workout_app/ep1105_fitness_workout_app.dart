a6f54e06a63b38c36f73580e0a3257c1f6503115
Dreamwalker <aristojeff@gmail.com>
2022-03-28 18:38:41 +0900

    Update ep1105_fitness_workout_app.dart

---
+++ lib/ep1105_fitness_workout_app/ep1105_fitness_workout_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class FitnessWorkoutApp extends StatelessWidget {
+  const FitnessWorkoutApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
