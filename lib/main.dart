9ffe77579ae0a3818579b5eb7dde1b5e288a1e21
Dreamwalker <aristojeff@gmail.com>
2022-03-28 18:35:55 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_19th_story/ep1101_mail_inbox_app/mail_inbox_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
 main() {
-  runApp(ProviderScope(child: const MailInboxApp()));
+  runApp(
+    const ProviderScope(
+      child: MailInboxApp(),
+    ),
+  );
 }
