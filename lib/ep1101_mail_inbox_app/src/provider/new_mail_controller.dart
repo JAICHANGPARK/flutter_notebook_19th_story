8880e696b27eac152aee325a7b3ad8c26b338c83
Dreamwalker <aristojeff@gmail.com>
2022-03-28 18:01:46 +0900

    Create new_mail_controller.dart

---
+++ lib/ep1101_mail_inbox_app/src/provider/new_mail_controller.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+final subjectInputProvider = Provider((ref)=> TextEditingController());
+final bodyInputProvider = Provider((ref)=> TextEditingController());
