bd16fb0289124bca836c754fcc1b00ebab4dbdc3
Dreamwalker <aristojeff@gmail.com>
2022-03-28 18:46:17 +0900

    Update fitness_workout_home_page.dart

--- lib/ep1105_fitness_workout_app/src/ui/fitness_workout_home_page.dart
+++ lib/ep1105_fitness_workout_app/src/ui/fitness_workout_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FitnessWorkoutHomePage extends StatefulWidget {
   const FitnessWorkoutHomePage({Key? key}) : super(key: key);
 
@@ -8,9 +7,64 @@ class FitnessWorkoutHomePage extends StatefulWidget {
   State<FitnessWorkoutHomePage> createState() => _FitnessWorkoutHomePageState();
 }
 
-class _FitnessWorkoutHomePageState extends State<FitnessWorkoutHomePage> {
+class _FitnessWorkoutHomePageState extends State<FitnessWorkoutHomePage>
+with SingleTickerProviderStateMixin{
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              title: Text("Live workouts"),
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.search),
+                ),
+              ],
+              bottom: TabBar(
+                controller: _tabController,
+                tabs: [
+                  Tab(text: "Live show"),
+                  Tab(text: "Watch Later",),
+                ],
+              ),
+            ),
+
+          ];
+        },
+        body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 240,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 240,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
