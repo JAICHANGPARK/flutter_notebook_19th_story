5bf8d3a1856fa2055bf1b08d51aec9d480017c15
Dreamwalker <aristojeff@gmail.com>
2022-04-02 20:10:00 +0900

    Update smart_home_main_page.dart

--- lib/ep1109_smart_home_app/src/ui/smart_home_main_page.dart
+++ lib/ep1109_smart_home_app/src/ui/smart_home_main_page.dart
@@ -1,6 +1,5 @@
-
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_19th_story/ep1109_smart_home_app/src/utils/theme.dart';
 
 class SmartHomeMainPage extends StatelessWidget {
   const SmartHomeMainPage({Key? key}) : super(key: key);
@@ -8,7 +7,39 @@ class SmartHomeMainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: kShaPrimary,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: kShaAccents,
+              ),
+              padding: EdgeInsets.symmetric(vertical: 12),
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "User12345",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Icon(
+                      Icons.arrow_drop_down,
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
