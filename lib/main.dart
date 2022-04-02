f055c51249001154cb4d8cc50c59e30ad3b13f76
Dreamwalker <aristojeff@gmail.com>
2022-04-02 20:04:48 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_19th_story/ep1105_fitness_workout_app/ep1105_fitness_workout_app.dart';
+import 'package:flutter_notebook_19th_story/ep1109_smart_home_app/smart_home_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 main() {
   runApp(
     const ProviderScope(
-      child: FitnessWorkoutApp(),
+      child: SmartHomeApp(),
     ),
   );
 }
