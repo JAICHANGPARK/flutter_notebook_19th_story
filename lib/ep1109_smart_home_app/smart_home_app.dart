772d3cdadae9f80749382cc16a6063ae7cd8a6cb
Dreamwalker <aristojeff@gmail.com>
2022-04-02 20:04:58 +0900

    Update smart_home_app.dart

--- lib/ep1109_smart_home_app/smart_home_app.dart
+++ lib/ep1109_smart_home_app/smart_home_app.dart
@@ -1,11 +1,15 @@
 
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'
+
+import 'src/ui/smart_home_main_page.dart';;
 
 class SmartHomeApp extends StatelessWidget {
   const SmartHomeApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: SmartHomeMainPage(),
+    );
   }
 }
