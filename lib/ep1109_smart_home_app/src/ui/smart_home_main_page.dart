d14efa80396cc9295017d9e5f46ee13e023c58e9
Dreamwalker <aristojeff@gmail.com>
2022-04-02 20:04:52 +0900

    Create smart_home_main_page.dart

---
+++ lib/ep1109_smart_home_app/src/ui/smart_home_main_page.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+
+class SmartHomeMainPage extends StatelessWidget {
+  const SmartHomeMainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
