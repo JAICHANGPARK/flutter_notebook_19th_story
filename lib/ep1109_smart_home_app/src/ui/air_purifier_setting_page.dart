79b437a1127213875c227a48c64f6654f2f8c276
Dreamwalker <aristojeff@gmail.com>
2022-04-03 18:07:57 +0900

    Create air_purifier_setting_page.dart

---
+++ lib/ep1109_smart_home_app/src/ui/air_purifier_setting_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AirPurifierSettingPage extends StatefulWidget {
+  const AirPurifierSettingPage({Key? key}) : super(key: key);
+
+  @override
+  State<AirPurifierSettingPage> createState() => _AirPurifierSettingPageState();
+}
+
+class _AirPurifierSettingPageState extends State<AirPurifierSettingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
