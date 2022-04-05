52120f473fda5614a7954ee6acc82b1c93efebfd
Dreamwalker <aristojeff@gmail.com>
2022-04-05 20:07:35 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_19th_story/ep1109_smart_home_app/smart_home_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'ep1114_e_commerce_filters_app/ep1114_e_commerce_filters_app.dart';
 
 main() {
   runApp(
     const ProviderScope(
-      child: SmartHomeApp(),
+      child: ECommerceFiltersApp(),
     ),
   );
 }
