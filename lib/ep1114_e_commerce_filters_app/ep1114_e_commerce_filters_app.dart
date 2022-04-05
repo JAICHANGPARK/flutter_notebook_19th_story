ba514c16e3618eec8b6211788b6feabbb4e72140
Dreamwalker <aristojeff@gmail.com>
2022-04-05 20:08:33 +0900

    Update ep1114_e_commerce_filters_app.dart

---
+++ lib/ep1114_e_commerce_filters_app/ep1114_e_commerce_filters_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_notebook_19th_story/ep1114_e_commerce_filters_app/src/ui/e_commerce_home_page.dart';
+
+class ECommerceFiltersApp extends StatelessWidget {
+  const ECommerceFiltersApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: EcommerceHomePage(),
+    );
+  }
+}
