1a44bc206a0c29105f3d8b85c1cb16e12bc19578
Dreamwalker <aristojeff@gmail.com>
2022-04-06 19:54:26 +0900

    Update shop_result_provider.dart

--- lib/ep1114_e_commerce_filters_app/src/provider/shop_result_provider.dart
+++ lib/ep1114_e_commerce_filters_app/src/provider/shop_result_provider.dart
@@ -1,12 +1,51 @@
-
-
 import 'package:flutter_notebook_19th_story/ep1114_e_commerce_filters_app/src/model/shop_results.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final shopResultProvider = StateNotifierProvider((ref) =>ShopResultController([]) );
-
-
-class ShopResultController extends StateNotifier<List<ShopResult>>{
-  ShopResultController(super.state);
-
-}
+final shopResultProvider = FutureProvider<List<ShopResult>>((ref) {
+  Future.delayed(const Duration(seconds: 3)).then((value) {
+    return [
+      ShopResult(
+        name: "2021 Apple 16-inch MacBook Pro",
+        price: "2,639.90",
+        isSoldOut: false,
+        vendor: "Apple",
+        img: "https://cdn.pixabay.com/photo/2015/01/08/18/24/children-593313_960_720.jpg",
+      ),
+      ShopResult(
+        name: "2021 Apple 16-inch MacBook Pro",
+        price: "2,639.90",
+        isSoldOut: true,
+        vendor: "Apple",
+        img: "https://cdn.pixabay.com/photo/2015/01/08/18/24/children-593313_960_720.jpg",
+      ),
+      ShopResult(
+        name: "2021 Apple 16-inch MacBook Pro",
+        price: "2,639.90",
+        isSoldOut: false,
+        vendor: "Apple",
+        img: "https://cdn.pixabay.com/photo/2015/01/08/18/24/children-593313_960_720.jpg",
+      ),
+      ShopResult(
+        name: "2021 Apple 16-inch MacBook Pro",
+        price: "2,639.90",
+        isSoldOut: false,
+        vendor: "Apple",
+        img: "https://cdn.pixabay.com/photo/2015/01/08/18/24/children-593313_960_720.jpg",
+      ),
+      ShopResult(
+        name: "2021 Apple 16-inch MacBook Pro",
+        price: "2,639.90",
+        isSoldOut: false,
+        vendor: "Apple",
+        img: "https://cdn.pixabay.com/photo/2015/01/08/18/24/children-593313_960_720.jpg",
+      ),
+      ShopResult(
+        name: "2021 Apple 16-inch MacBook Pro",
+        price: "2,639.90",
+        isSoldOut: false,
+        vendor: "Apple",
+        img: "https://cdn.pixabay.com/photo/2015/01/08/18/24/children-593313_960_720.jpg",
+      ),
+    ];
+  });
+});
