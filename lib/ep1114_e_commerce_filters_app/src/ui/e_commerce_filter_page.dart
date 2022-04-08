6cf95fe29d9d5c167aef65f8f5b3afe7de0218eb
Dreamwalker <aristojeff@gmail.com>
2022-04-08 19:53:38 +0900

    Create e_commerce_filter_page.dart

---
+++ lib/ep1114_e_commerce_filters_app/src/ui/e_commerce_filter_page.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class ECommerceFilterPage extends StatefulWidget {
+  const ECommerceFilterPage({Key? key}) : super(key: key);
+
+  @override
+  State<ECommerceFilterPage> createState() => _ECommerceFilterPageState();
+}
+
+class _ECommerceFilterPageState extends State<ECommerceFilterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
