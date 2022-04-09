5202325da472fb39be6b1447b6c935ceb0866c1f
Dreamwalker <aristojeff@gmail.com>
2022-04-09 17:35:36 +0900

    Update staking_main_page.dart

--- lib/ep1117_staking_app/src/ui/staking_main_page.dart
+++ lib/ep1117_staking_app/src/ui/staking_main_page.dart
@@ -1,1 +1,17 @@
 
+
+import 'package:flutter/material.dart';
+
+class StakingMainPage extends StatefulWidget {
+  const StakingMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<StakingMainPage> createState() => _StakingMainPageState();
+}
+
+class _StakingMainPageState extends State<StakingMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
