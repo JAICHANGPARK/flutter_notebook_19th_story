5680ace172c9b0d7c1b8b233b58754156f0d24db
Dreamwalker <aristojeff@gmail.com>
2022-04-09 17:33:38 +0900

    Create staking_app.dart

---
+++ lib/ep1117_staking_app/staking_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StakingApp extends StatelessWidget {
+  const StakingApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
