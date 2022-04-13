e31f260c2d9192b554ee4f77eb6bb9d47b97212f
Dreamwalker <aristojeff@gmail.com>
2022-04-13 20:31:29 +0900

    Create crypto_invest_page.dart

---
+++ lib/ep1121_crypto_analysis_app/src/ui/crypto_invest_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CryptoInvestPage extends StatefulWidget {
+  const CryptoInvestPage({Key? key}) : super(key: key);
+
+  @override
+  State<CryptoInvestPage> createState() => _CryptoInvestPageState();
+}
+
+class _CryptoInvestPageState extends State<CryptoInvestPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
