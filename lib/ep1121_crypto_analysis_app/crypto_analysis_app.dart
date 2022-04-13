8c76aba153a5aa52300c04690d679f5b2a566080
Dreamwalker <aristojeff@gmail.com>
2022-04-13 20:31:34 +0900

    Update crypto_analysis_app.dart

--- lib/ep1121_crypto_analysis_app/crypto_analysis_app.dart
+++ lib/ep1121_crypto_analysis_app/crypto_analysis_app.dart
@@ -1,11 +1,14 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_19th_story/ep1121_crypto_analysis_app/src/ui/crypto_invest_page.dart';
 
 class CryptoAnalysisApp extends StatelessWidget {
   const CryptoAnalysisApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: CryptoInvestPage(),
+    );
   }
 }
