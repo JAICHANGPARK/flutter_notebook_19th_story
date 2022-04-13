94009dae0b91161318eb607fd9929ac54d7a2dd4
Dreamwalker <aristojeff@gmail.com>
2022-04-13 20:29:49 +0900

    Update crypto_analysis_app.dart

---
+++ lib/ep1121_crypto_analysis_app/crypto_analysis_app.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class CryptoAnalysisApp extends StatelessWidget {
+  const CryptoAnalysisApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
