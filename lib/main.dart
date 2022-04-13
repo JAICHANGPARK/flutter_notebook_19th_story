7c7d97fb6823329c98638a90e8daff2bf660430e
Dreamwalker <aristojeff@gmail.com>
2022-04-13 20:31:24 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_19th_story/ep1117_staking_app/staking_app.dart';
+import 'package:flutter_notebook_19th_story/ep1121_crypto_analysis_app/crypto_analysis_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 main() {
   runApp(
     const ProviderScope(
-      child: StakingApp(),
+      child: CryptoAnalysisApp(),
     ),
   );
 }
