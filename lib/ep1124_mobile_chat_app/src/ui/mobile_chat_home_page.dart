dac1ab07df1754203789b9842916d75dc0bcecb6
Dreamwalker <aristojeff@gmail.com>
2022-04-16 16:53:59 +0900

    Update mobile_chat_home_page.dart

---
+++ lib/ep1124_mobile_chat_app/src/ui/mobile_chat_home_page.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class MobileChatHomePage extends StatefulWidget {
+  const MobileChatHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MobileChatHomePage> createState() => _MobileChatHomePageState();
+}
+
+class _MobileChatHomePageState extends State<MobileChatHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
