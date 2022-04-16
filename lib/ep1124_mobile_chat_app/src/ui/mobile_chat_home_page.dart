1ecff38b49e659af922aeab9e5467a1e463632a8
Dreamwalker <aristojeff@gmail.com>
2022-04-16 17:02:17 +0900

    Update mobile_chat_home_page.dart

--- lib/ep1124_mobile_chat_app/src/ui/mobile_chat_home_page.dart
+++ lib/ep1124_mobile_chat_app/src/ui/mobile_chat_home_page.dart
@@ -9,55 +9,61 @@ class MobileChatHomePage extends StatefulWidget {
 
 class _MobileChatHomePageState extends State<MobileChatHomePage> {
   int _bottomTabIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-
       body: Stack(
         children: [
-          Positioned(child: Container(
-            height: MediaQuery.of(context).size.height / 2 ,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topRight: Radius.circular(16),
-                topLeft: Radius.circular(16)
-              )
+          Positioned(
+            child: Container(
+              height: MediaQuery.of(context).size.height / 2,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topRight: Radius.circular(16), topLeft: Radius.circular(16))),
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Messages",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          left: 4,
+            left: 4,
             right: 4,
             bottom: 0,
           )
         ],
       ),
-
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _bottomTabIndex,
-        onTap: (idx){
-          setState((){
+        onTap: (idx) {
+          setState(() {
             _bottomTabIndex = idx;
           });
         },
-        items:const [
-           BottomNavigationBarItem(icon:  Icon(Icons.send), label: "Messages"),
-           BottomNavigationBarItem(icon:  Icon(Icons.call), label: "Call"),
-           BottomNavigationBarItem(icon:  Icon(Icons.settings), label: "Settings"),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.send), label: "Messages"),
+          BottomNavigationBarItem(icon: Icon(Icons.call), label: "Call"),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings"),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-
         backgroundColor: Colors.black,
-        onPressed: () {  },
+        onPressed: () {},
         child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(4)
+          decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(4)),
+          child: Icon(
+            Icons.edit,
+            color: Colors.black,
           ),
-          child: Icon(Icons.edit, color: Colors.black,),
         ),
-
       ),
     );
   }
