0152df9ea13a0c6bbf29596489b9c2b45e7dc2d0
Dreamwalker <aristojeff@gmail.com>
2022-04-16 16:52:42 +0900

    Update mobile_chat_app.dart

---
+++ lib/ep1124_mobile_chat_app/mobile_chat_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MobileChatApp extends StatelessWidget {
+  const MobileChatApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
