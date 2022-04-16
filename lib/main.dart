45d8f0f7604d20d2faaad08e64c66f58ed1e37d4
Dreamwalker <aristojeff@gmail.com>
2022-04-16 16:54:26 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_19th_story/ep1121_crypto_analysis_app/crypto_analysis_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'ep1124_mobile_chat_app/mobile_chat_app.dart';
 
 main() {
   runApp(
     const ProviderScope(
-      child: CryptoAnalysisApp(),
+      child: MobileChatApp(),
     ),
   );
 }
