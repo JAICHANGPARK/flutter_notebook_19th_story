584536a41065a4c5cd65ba8457003a4c0648cbc6
Dreamwalker <aristojeff@gmail.com>
2022-04-19 19:20:20 +0900

    Update monitoring_main_page.dart

--- lib/ep1127_patient_monitoring_app/src/ui/monitoring_main_page.dart
+++ lib/ep1127_patient_monitoring_app/src/ui/monitoring_main_page.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class MonitoringMainPage extends StatefulWidget {
@@ -13,12 +11,31 @@ class _MonitoringMainPageState extends State<MonitoringMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-
-          ],
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Good Morning",
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.w300,),
+                      ),
+                      Text(
+                        "Dreamwalker",
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w800),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
