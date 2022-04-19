fb3677bed1a6643d74d6634973ca105e232d669a
Dreamwalker <aristojeff@gmail.com>
2022-04-19 19:17:40 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_19th_story/ep1127_patient_monitoring_app/patient_monitoring_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'ep1124_mobile_chat_app/mobile_chat_app.dart';
 
 main() {
   runApp(
     const ProviderScope(
-      child: MobileChatApp(),
+      child: PatientMonitoringApp(),
     ),
   );
 }
