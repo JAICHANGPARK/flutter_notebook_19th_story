d10b98117d8a219c89d14dd6da56d84e47fedd0e
Dreamwalker <aristojeff@gmail.com>
2022-04-19 19:22:24 +0900

    Update monitoring_main_page.dart

--- lib/ep1127_patient_monitoring_app/src/ui/monitoring_main_page.dart
+++ lib/ep1127_patient_monitoring_app/src/ui/monitoring_main_page.dart
@@ -14,27 +14,58 @@ class _MonitoringMainPageState extends State<MonitoringMainPage> {
       backgroundColor: Colors.white,
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Good Morning",
-                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.w300,),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Good Morning",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          "Dreamwalker",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Spacer(),
+                    Container(
+                      height: 48,
+                      width: 48,
+                      decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(8),
                       ),
-                      Text(
-                        "Dreamwalker",
-                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w800),
-                      )
-                    ],
-                  )
-                ],
-              )
-            ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  height: 54,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
