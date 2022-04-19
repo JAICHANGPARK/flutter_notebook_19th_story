dd065003e5c2032b802063c0e1947a2eeb138d53
Dreamwalker <aristojeff@gmail.com>
2022-04-19 19:16:41 +0900

    Create monitoring_main_page.dart

---
+++ lib/ep1127_patient_monitoring_app/src/ui/monitoring_main_page.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class MonitoringMainPage extends StatefulWidget {
+  const MonitoringMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MonitoringMainPage> createState() => _MonitoringMainPageState();
+}
+
+class _MonitoringMainPageState extends State<MonitoringMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
