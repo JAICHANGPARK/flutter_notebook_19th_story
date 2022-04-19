1469c22441ba8fca2fe4eebd5de9632549bca74a
Dreamwalker <aristojeff@gmail.com>
2022-04-19 19:18:20 +0900

    Update patient_monitoring_app.dart

--- lib/ep1127_patient_monitoring_app/patient_monitoring_app.dart
+++ lib/ep1127_patient_monitoring_app/patient_monitoring_app.dart
@@ -1,11 +1,14 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_19th_story/ep1127_patient_monitoring_app/src/ui/monitoring_main_page.dart';
 
 class PatientMonitoringApp extends StatelessWidget {
   const PatientMonitoringApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: MonitoringMainPage(),
+    );
   }
 }
