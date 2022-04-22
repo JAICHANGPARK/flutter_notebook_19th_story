314e0ceb86d797f4c06611bc9edaac33a69c9bc3
Dreamwalker <aristojeff@gmail.com>
2022-04-22 20:02:19 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_19th_story/ep1127_patient_monitoring_app/patient_monitoring_app.dart';
+import 'package:flutter_notebook_19th_story/ep1130_classified_app/classified_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 main() {
   runApp(
     const ProviderScope(
-      child: PatientMonitoringApp(),
+      child: ClassifiedApp(),
     ),
   );
 }
