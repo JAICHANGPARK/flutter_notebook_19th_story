e15bad46715cf7fee06283cae0d271ee98167b93
Dreamwalker <aristojeff@gmail.com>
2022-04-22 20:09:57 +0900

    Update class_hotel_page.dart

--- lib/ep1130_classified_app/src/class_hotel_page.dart
+++ lib/ep1130_classified_app/src/class_hotel_page.dart
@@ -28,60 +28,104 @@ class _ClassHotelPageState extends State<ClassHotelPage> {
                     ),
                   ),
                 )),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
-                    children: const [
-                      CircleAvatar(
-                        backgroundColor: Colors.white,
-                        foregroundColor: Colors.black,
-                        child: Center(
-                          child: Icon(
-                            Icons.arrow_back_ios,
-                            size: 16,
+            Positioned(
+              left: 0,
+              top: 0,
+              bottom: 0,
+              right: 0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: const [
+                        CircleAvatar(
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.black,
+                          child: Center(
+                            child: Icon(
+                              Icons.arrow_back_ios,
+                              size: 16,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 38,
+                        width: 4,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(3),
+                            bottomRight: Radius.circular(3),
                           ),
                         ),
                       ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                Row(
-                  children: [
-                    Container(
-                      height: 38,
-                      width: 4,
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(3),
-                          bottomRight: Radius.circular(3),
+                      SizedBox(
+                        width: 24,
+                      ),
+                      Text(
+                        "Nearby Hotels",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width: 24,
-                    ),
-                    Text(
-                      "Nearby Hotels",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
+                      Spacer(),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text("See All"),
                       ),
-                    ),
-                    Spacer(),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text("See All"),
-                    ),
-                  ],
-                )
-              ],
+                    ],
+                  ),
+                  SizedBox(
+                    height: 280,
+                    child: Placeholder(),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 38,
+                        width: 4,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(3),
+                            bottomRight: Radius.circular(3),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 24,
+                      ),
+                      Text(
+                        "Other Places",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Spacer(),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text("See All"),
+                      ),
+                    ],
+                  ),
+                  Expanded(child: Placeholder())
+                ],
+              ),
             ),
           ],
         ),
