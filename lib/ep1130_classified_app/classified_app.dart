bdb656af6909b74759abe0e1766183a5193a2afc
Dreamwalker <aristojeff@gmail.com>
2022-04-22 20:00:51 +0900

    Update classified_app.dart

--- lib/ep1130_classified_app/classified_app.dart
+++ lib/ep1130_classified_app/classified_app.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'src/class_hotel_page.dart';
+
 class ClassifiedApp extends StatelessWidget {
   const ClassifiedApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: ClassHotelPage(),
+    );
   }
 }
