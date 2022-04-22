66bb3f193abe7c122ded51fc47a227838a80b86d
Dreamwalker <aristojeff@gmail.com>
2022-04-22 20:00:37 +0900

    Create class_hotel_page.dart

---
+++ lib/ep1130_classified_app/src/class_hotel_page.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:flutter/material.dart';
+
+class ClassHotelPage extends StatefulWidget {
+  const ClassHotelPage({Key? key}) : super(key: key);
+
+  @override
+  State<ClassHotelPage> createState() => _ClassHotelPageState();
+}
+
+class _ClassHotelPageState extends State<ClassHotelPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
