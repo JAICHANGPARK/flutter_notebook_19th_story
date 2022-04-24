843abf75ddf3b3b9db229f560ae3bc4380f73450
Dreamwalker <aristojeff@gmail.com>
2022-04-24 14:14:41 +0900

    Create fake_other_places.dart

---
+++ lib/ep1130_classified_app/src/mock/fake_other_places.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_notebook_19th_story/ep1130_classified_app/src/model/other_places.dart';
+
+List<OtherPlace> fakeOtherPlaceItems = [
+  OtherPlace(
+    img: "https://cdn.pixabay.com/photo/2019/08/19/13/58/bed-4416515_960_720.jpg",
+    title: "The Grand Hotel",
+    price: "75",
+    isLike: false,
+    review: "3.8",
+  ),
+  OtherPlace(
+    img: "https://cdn.pixabay.com/photo/2014/07/21/19/20/lobby-398845__340.jpg",
+    title: "Blue Ocean Hotel",
+    price: "86",
+    isLike: true,
+    review: "4.2",
+  ),
+  OtherPlace(
+    img: "https://cdn.pixabay.com/photo/2014/07/10/17/17/hotel-389256__340.jpg",
+    title: "The Grand Hotel",
+    price: "75",
+    isLike: false,
+    review: "3.8",
+  ),
+  OtherPlace(
+    img: "https://cdn.pixabay.com/photo/2014/07/10/17/17/hotel-389256__340.jpg",
+    title: "The Grand Hotel",
+    price: "75",
+    isLike: false,
+    review: "3.8",
+  ),
+  OtherPlace(
+    img: "https://cdn.pixabay.com/photo/2014/07/10/17/17/hotel-389256__340.jpg",
+    title: "The Grand Hotel",
+    price: "75",
+    isLike: false,
+    review: "3.8",
+  )
+];
