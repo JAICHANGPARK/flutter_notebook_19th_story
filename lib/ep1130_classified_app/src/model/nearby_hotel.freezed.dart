1a33b679778d89588e8d689d509abd64dbdb1e1e
Dreamwalker <aristojeff@gmail.com>
2022-04-25 20:36:21 +0900

    Create nearby_hotel.freezed.dart

---
+++ lib/ep1130_classified_app/src/model/nearby_hotel.freezed.dart
@@ -0,0 +1,241 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'nearby_hotel.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$NearbyHotelTearOff {
+  const _$NearbyHotelTearOff();
+
+  _NearbyHotel call(
+      {String? img,
+      String? title,
+      String? price,
+      String? review,
+      List<String> imgItems = const []}) {
+    return _NearbyHotel(
+      img: img,
+      title: title,
+      price: price,
+      review: review,
+      imgItems: imgItems,
+    );
+  }
+}
+
+/// @nodoc
+const $NearbyHotel = _$NearbyHotelTearOff();
+
+/// @nodoc
+mixin _$NearbyHotel {
+  String? get img => throw _privateConstructorUsedError;
+  String? get title => throw _privateConstructorUsedError;
+  String? get price => throw _privateConstructorUsedError;
+  String? get review => throw _privateConstructorUsedError;
+  List<String> get imgItems => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $NearbyHotelCopyWith<NearbyHotel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $NearbyHotelCopyWith<$Res> {
+  factory $NearbyHotelCopyWith(
+          NearbyHotel value, $Res Function(NearbyHotel) then) =
+      _$NearbyHotelCopyWithImpl<$Res>;
+  $Res call(
+      {String? img,
+      String? title,
+      String? price,
+      String? review,
+      List<String> imgItems});
+}
+
+/// @nodoc
+class _$NearbyHotelCopyWithImpl<$Res> implements $NearbyHotelCopyWith<$Res> {
+  _$NearbyHotelCopyWithImpl(this._value, this._then);
+
+  final NearbyHotel _value;
+  // ignore: unused_field
+  final $Res Function(NearbyHotel) _then;
+
+  @override
+  $Res call({
+    Object? img = freezed,
+    Object? title = freezed,
+    Object? price = freezed,
+    Object? review = freezed,
+    Object? imgItems = freezed,
+  }) {
+    return _then(_value.copyWith(
+      img: img == freezed
+          ? _value.img
+          : img // ignore: cast_nullable_to_non_nullable
+              as String?,
+      title: title == freezed
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String?,
+      price: price == freezed
+          ? _value.price
+          : price // ignore: cast_nullable_to_non_nullable
+              as String?,
+      review: review == freezed
+          ? _value.review
+          : review // ignore: cast_nullable_to_non_nullable
+              as String?,
+      imgItems: imgItems == freezed
+          ? _value.imgItems
+          : imgItems // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$NearbyHotelCopyWith<$Res>
+    implements $NearbyHotelCopyWith<$Res> {
+  factory _$NearbyHotelCopyWith(
+          _NearbyHotel value, $Res Function(_NearbyHotel) then) =
+      __$NearbyHotelCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {String? img,
+      String? title,
+      String? price,
+      String? review,
+      List<String> imgItems});
+}
+
+/// @nodoc
+class __$NearbyHotelCopyWithImpl<$Res> extends _$NearbyHotelCopyWithImpl<$Res>
+    implements _$NearbyHotelCopyWith<$Res> {
+  __$NearbyHotelCopyWithImpl(
+      _NearbyHotel _value, $Res Function(_NearbyHotel) _then)
+      : super(_value, (v) => _then(v as _NearbyHotel));
+
+  @override
+  _NearbyHotel get _value => super._value as _NearbyHotel;
+
+  @override
+  $Res call({
+    Object? img = freezed,
+    Object? title = freezed,
+    Object? price = freezed,
+    Object? review = freezed,
+    Object? imgItems = freezed,
+  }) {
+    return _then(_NearbyHotel(
+      img: img == freezed
+          ? _value.img
+          : img // ignore: cast_nullable_to_non_nullable
+              as String?,
+      title: title == freezed
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String?,
+      price: price == freezed
+          ? _value.price
+          : price // ignore: cast_nullable_to_non_nullable
+              as String?,
+      review: review == freezed
+          ? _value.review
+          : review // ignore: cast_nullable_to_non_nullable
+              as String?,
+      imgItems: imgItems == freezed
+          ? _value.imgItems
+          : imgItems // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_NearbyHotel implements _NearbyHotel {
+  _$_NearbyHotel(
+      {this.img,
+      this.title,
+      this.price,
+      this.review,
+      this.imgItems = const []});
+
+  @override
+  final String? img;
+  @override
+  final String? title;
+  @override
+  final String? price;
+  @override
+  final String? review;
+  @JsonKey()
+  @override
+  final List<String> imgItems;
+
+  @override
+  String toString() {
+    return 'NearbyHotel(img: $img, title: $title, price: $price, review: $review, imgItems: $imgItems)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _NearbyHotel &&
+            const DeepCollectionEquality().equals(other.img, img) &&
+            const DeepCollectionEquality().equals(other.title, title) &&
+            const DeepCollectionEquality().equals(other.price, price) &&
+            const DeepCollectionEquality().equals(other.review, review) &&
+            const DeepCollectionEquality().equals(other.imgItems, imgItems));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(img),
+      const DeepCollectionEquality().hash(title),
+      const DeepCollectionEquality().hash(price),
+      const DeepCollectionEquality().hash(review),
+      const DeepCollectionEquality().hash(imgItems));
+
+  @JsonKey(ignore: true)
+  @override
+  _$NearbyHotelCopyWith<_NearbyHotel> get copyWith =>
+      __$NearbyHotelCopyWithImpl<_NearbyHotel>(this, _$identity);
+}
+
+abstract class _NearbyHotel implements NearbyHotel {
+  factory _NearbyHotel(
+      {String? img,
+      String? title,
+      String? price,
+      String? review,
+      List<String> imgItems}) = _$_NearbyHotel;
+
+  @override
+  String? get img;
+  @override
+  String? get title;
+  @override
+  String? get price;
+  @override
+  String? get review;
+  @override
+  List<String> get imgItems;
+  @override
+  @JsonKey(ignore: true)
+  _$NearbyHotelCopyWith<_NearbyHotel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
