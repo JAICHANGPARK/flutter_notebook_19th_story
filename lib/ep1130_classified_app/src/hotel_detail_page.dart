462b580b0e6cb128b9dc2a6f5f418aa889f37ada
Dreamwalker <aristojeff@gmail.com>
2022-04-25 20:41:09 +0900

    Update hotel_detail_page.dart

--- lib/ep1130_classified_app/src/hotel_detail_page.dart
+++ lib/ep1130_classified_app/src/hotel_detail_page.dart
@@ -1,12 +1,11 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_19th_story/ep1130_classified_app/src/model/nearby_hotel.dart';
 import 'package:flutter_notebook_19th_story/ep1130_classified_app/src/model/other_places.dart';
 
 class HotelDetailPage extends StatefulWidget {
-  final OtherPlace otherPlace;
-  const HotelDetailPage({Key? key,
-    required this.otherPlace}) : super(key: key);
+  final NearbyHotel nearbyHotel;
+
+  const HotelDetailPage({Key? key, required this.nearbyHotel}) : super(key: key);
 
   @override
   State<HotelDetailPage> createState() => _HotelDetailPageState();
@@ -15,6 +14,40 @@ class HotelDetailPage extends StatefulWidget {
 class _HotelDetailPageState extends State<HotelDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(child: Placeholder()),
+          Expanded(child: Placeholder()),
+          Container(
+            height: 64,
+            child: Row(
+              children: [
+                Expanded(
+                    child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [Text("Total Amount:"), Text("\$${widget.nearbyHotel.price}")],
+                )),
+                Expanded(
+                    child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Book Now",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                )),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
