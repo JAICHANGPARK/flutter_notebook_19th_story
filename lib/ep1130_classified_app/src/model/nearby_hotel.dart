3598e662f985c774f94ebfebc979532fff46b2a4
Dreamwalker <aristojeff@gmail.com>
2022-04-25 20:32:36 +0900

    Create nearby_hotel.dart

---
+++ lib/ep1130_classified_app/src/model/nearby_hotel.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'nearby_hotel.freezed.dart';
+
+@freezed
+class NearbyHotel with _$NearbyHotel {
+  factory NearbyHotel({
+    String? img,
+    String? title,
+    String? price,
+    String? review,
+    @Default([]) List<String> imgItems,
+  }) = _NearbyHotel;
+}
