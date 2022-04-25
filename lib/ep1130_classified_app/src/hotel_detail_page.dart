3bf6578586a404b05ce80fdd12e9355bcf05426b
Dreamwalker <aristojeff@gmail.com>
2022-04-25 20:32:45 +0900

    Update hotel_detail_page.dart

--- lib/ep1130_classified_app/src/hotel_detail_page.dart
+++ lib/ep1130_classified_app/src/hotel_detail_page.dart
@@ -1,3 +1,20 @@
 
 
-s
+import 'package:flutter/material.dart';
+import 'package:flutter_notebook_19th_story/ep1130_classified_app/src/model/other_places.dart';
+
+class HotelDetailPage extends StatefulWidget {
+  final OtherPlace otherPlace;
+  const HotelDetailPage({Key? key,
+    required this.otherPlace}) : super(key: key);
+
+  @override
+  State<HotelDetailPage> createState() => _HotelDetailPageState();
+}
+
+class _HotelDetailPageState extends State<HotelDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
