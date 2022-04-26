a35ef4acfdea6259dd153a0c14fc4964ec3e607e
Dreamwalker <aristojeff@gmail.com>
2022-04-26 20:14:35 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_19th_story/ep1130_classified_app/classified_app.dart';
+import 'package:flutter_notebook_19th_story/ep1134_podcast_app/podcast_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 main() {
   runApp(
     const ProviderScope(
-      child: ClassifiedApp(),
+      child: PodcastApp(),
     ),
   );
 }
