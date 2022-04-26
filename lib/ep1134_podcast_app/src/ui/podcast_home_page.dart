1c8b0e3ab68f6c87f3c2332b4bd0b9c958419665
Dreamwalker <aristojeff@gmail.com>
2022-04-26 20:14:39 +0900

    Create podcast_home_page.dart

---
+++ lib/ep1134_podcast_app/src/ui/podcast_home_page.dart
@@ -0,0 +1,15 @@
+
+
+class PodcastHomePage extends StatefulWidget {
+  const PodcastHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<PodcastHomePage> createState() => _PodcastHomePageState();
+}
+
+class _PodcastHomePageState extends State<PodcastHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
