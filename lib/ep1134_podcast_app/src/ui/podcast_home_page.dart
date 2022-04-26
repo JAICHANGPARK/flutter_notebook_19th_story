5a98621b7c16859b8670f5def48c63f84e79f5c3
Dreamwalker <aristojeff@gmail.com>
2022-04-26 20:18:27 +0900

    Update podcast_home_page.dart

--- lib/ep1134_podcast_app/src/ui/podcast_home_page.dart
+++ lib/ep1134_podcast_app/src/ui/podcast_home_page.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class PodcastHomePage extends StatefulWidget {
@@ -12,6 +10,28 @@ class PodcastHomePage extends StatefulWidget {
 class _PodcastHomePageState extends State<PodcastHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text("Hi Dream 👋"),
+                    CircleAvatar(
+                      backgroundColor: Colors.purple.withOpacity(0.5),
+                      foregroundColor: Colors.purple,
+                      child: Icon(Icons.notifications_outlined,),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
