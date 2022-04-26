9d9ad04e8eed41c29bac85e1795747cd0b535835
Dreamwalker <aristojeff@gmail.com>
2022-04-26 20:16:31 +0900

    Update podcast_app.dart

--- lib/ep1134_podcast_app/podcast_app.dart
+++ lib/ep1134_podcast_app/podcast_app.dart
@@ -2,13 +2,15 @@
 
 import 'package:flutter/material.dart';
 
+import 'src/ui/podcast_home_page.dart';
+
 class PodcastApp extends StatelessWidget {
   const PodcastApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: PodcastHomePage(),
     );
   }
 }
