59326d9e2d8d779f4f39db7b81dc55d10660ed69
Dreamwalker <aristojeff@gmail.com>
2022-04-27 20:18:34 +0900

    Create top_category.dart

---
+++ lib/ep1134_podcast_app/src/model/top_category.dart
@@ -0,0 +1,30 @@
+class TopCategory{
+  String? icon;
+  String? title;
+
+  TopCategory({this.icon, this.title});
+}
+
+List<TopCategory> categories = [
+  TopCategory(
+    icon: "🐶",
+    title: "Pets",
+  ),
+  TopCategory(
+    icon: "🐶",
+    title: "Yoga",
+  ),
+  TopCategory(
+    icon: "🐶",
+    title: "Travel",
+  ),
+  TopCategory(
+    icon: "🐶",
+    title: "Eating",
+  ),
+  TopCategory(
+    icon: "🐶",
+    title: "Math",
+  ),
+
+];
