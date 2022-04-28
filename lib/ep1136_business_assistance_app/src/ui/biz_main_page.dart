7cb9bb0d8c13f2e1983795c4472cee1e0fa99d15
Dreamwalker <aristojeff@gmail.com>
2022-04-28 21:55:31 +0900

    Create biz_main_page.dart

---
+++ lib/ep1136_business_assistance_app/src/ui/biz_main_page.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class BizMainPage extends StatefulWidget {
+  const BizMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<BizMainPage> createState() => _BizMainPageState();
+}
+
+class _BizMainPageState extends State<BizMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
