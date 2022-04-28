66a8324bb0312160324502f7078e7a46bc0a2806
Dreamwalker <aristojeff@gmail.com>
2022-04-28 21:55:36 +0900

    Update biz_assist_app.dart

---
+++ lib/ep1136_business_assistance_app/biz_assist_app.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_notebook_19th_story/ep1136_business_assistance_app/src/ui/biz_main_page.dart';
+
+class BusinessAssistanceApp extends StatelessWidget {
+  const BusinessAssistanceApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BizMainPage(),
+    );
+  }
+}
