59c0638089ea2b4d29dc244d3684439a6139eb3a
Dreamwalker <aristojeff@gmail.com>
2022-04-29 20:32:24 +0900

    Update cashflows_provider.dart

--- lib/ep1136_business_assistance_app/src/provider/cashflows_provider.dart
+++ lib/ep1136_business_assistance_app/src/provider/cashflows_provider.dart
@@ -1,7 +1,21 @@
+import 'dart:convert';
+
 import 'package:flutter_notebook_19th_story/ep1136_business_assistance_app/src/model/cashflow.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final cashFlowProvider = FutureProvider<List<Cashflow>>((ref) async {
-  await Future.delayed(Duration(seconds: 3));
-  retunr
+String cashflows = """
+{
+"items" : [
+  {
+  "title": "total",
+  "price" : "2,343,538"
+  }
+]
+}
+
+""";
+
+final cashFlowProvider = FutureProvider<CashFlowItems>((ref) async {
+  await Future.delayed(const Duration(seconds: 3));
+  return CashFlowItems.fromJson(jsonDecode(cashflows));
 });
