ae0514a0260d7bc8dd6111b777476cf428257f92
Dreamwalker <aristojeff@gmail.com>
2022-04-29 20:26:39 +0900

    Create cashflows_provider.dart

---
+++ lib/ep1136_business_assistance_app/src/provider/cashflows_provider.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_notebook_19th_story/ep1136_business_assistance_app/src/model/cashflow.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final cashFlowProvider = FutureProvider<List<Cashflow>>((ref) async {
+  await Future.delayed(Duration(seconds: 3));
+  retunr
+});
