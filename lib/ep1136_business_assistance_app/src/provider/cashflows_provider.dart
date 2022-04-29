9af98a8fb90f5fd39e01573dce86989cb9039fed
Dreamwalker <aristojeff@gmail.com>
2022-04-29 20:39:33 +0900

    Update cashflows_provider.dart

--- lib/ep1136_business_assistance_app/src/provider/cashflows_provider.dart
+++ lib/ep1136_business_assistance_app/src/provider/cashflows_provider.dart
@@ -9,6 +9,38 @@ String cashflows = """
   {
   "title": "total",
   "price" : "2,343,538"
+  },
+    {
+  "title": "main",
+  "price" : "1,432,674"
+  },
+  {
+  "title": "for taxing",
+  "price" : "343,538"
+  },
+  {
+  "title": "investments",
+  "price" : "343,538"
+  },
+  {
+  "title": "rent",
+  "price" : "343,538"
+  },
+  {
+  "title": "private",
+  "price" : "343,538"
+  },
+  {
+  "title": "employee salaries",
+  "price" : "465,456"
+  },
+   {
+  "title": "online shopping",
+  "price" : "343,538"
+  },
+   {
+  "title": "reserved",
+  "price" : "343,538"
   }
 ]
 }
