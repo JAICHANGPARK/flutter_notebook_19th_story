ea7edbb549101065efda316fdf7ecec09e0b4225
Dreamwalker <aristojeff@gmail.com>
2022-05-01 15:53:24 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'ep1136_business_assistance_app/biz_assist_app.dart';
+import 'ep1139_office_meeting_app/office_meeting_app.dart';
 
 main() {
   runApp(
     const ProviderScope(
-      child: BusinessAssistanceApp(),
+      child: OfficeMeetingApp(),
     ),
   );
 }
