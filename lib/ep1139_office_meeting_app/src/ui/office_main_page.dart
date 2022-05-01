401bda0c42fda8d2d18417e445727a561e293d82
Dreamwalker <aristojeff@gmail.com>
2022-05-01 15:53:13 +0900

    Create office_main_page.dart

---
+++ lib/ep1139_office_meeting_app/src/ui/office_main_page.dart
@@ -0,0 +1,19 @@
+
+
+
+
+import 'package:flutter/material.dart';
+
+class OfficeMainPage extends StatefulWidget {
+  const OfficeMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<OfficeMainPage> createState() => _OfficeMainPageState();
+}
+
+class _OfficeMainPageState extends State<OfficeMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
