eccfd41893d811ecd102fdab8a1e432423f2942a
Dreamwalker <aristojeff@gmail.com>
2022-05-01 16:02:48 +0900

    Update office_main_page.dart

--- lib/ep1139_office_meeting_app/src/ui/office_main_page.dart
+++ lib/ep1139_office_meeting_app/src/ui/office_main_page.dart
@@ -14,86 +14,118 @@ class _OfficeMainPageState extends State<OfficeMainPage> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 24),
-                child: Row(
-                  children: [
-                    Container(
-                      height: 24,
-                      width: 24,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.blueAccent,
+          child: DefaultTabController(
+            length: 3,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 24),
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 24,
+                        width: 24,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.blueAccent,
+                        ),
+                        child: const Icon(
+                          Icons.keyboard_arrow_left,
+                          color: Colors.white,
+                        ),
                       ),
-                      child: const Icon(
-                        Icons.keyboard_arrow_left,
-                        color: Colors.white,
-                      ),
-                    ),
-                    const Expanded(
-                      child: Center(
-                        child: Text(
-                          "Meeting Schedule",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
+                      const Expanded(
+                        child: Center(
+                          child: Text(
+                            "Meeting Schedule",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 36,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Hello Dreamwalker",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                      decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: Row(
+                        children: const [
+                          Icon(
+                            Icons.add,
+                            color: Colors.white,
+                            size: 16,
+                          ),
+                          SizedBox(
+                            width: 4,
+                          ),
+                          Text(
+                            "Create Meeting",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                const Text(
+                  "Lets Get Started",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const TabBar(
+                  indicatorSize: TabBarIndicatorSize.label,
+                  labelColor: Colors.blueAccent,
+                  unselectedLabelColor: Colors.grey,
+                  tabs: [
+                    Tab(
+                      text: "Todays",
+                    ),
+                    Tab(
+                      text: "Tommorows",
+                    ),
+                    Tab(
+                      text: "Weekly",
                     ),
                   ],
                 ),
-              ),
-              const SizedBox(
-                height: 36,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    "Hello Dreamwalker",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
+                Expanded(
+                  child: TabBarView(
+                    children: [
+                      Container(),
+                      Container(),
+                      Container(),
+                    ],
                   ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                    decoration: BoxDecoration(
-                      color: Colors.blueAccent,
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    child: Row(
-                      children: const [
-                        Icon(
-                          Icons.add,
-                          color: Colors.white,
-                          size: 16,
-                        ),
-                        SizedBox(
-                          width: 4,
-                        ),
-                        Text(
-                          "Create Meeting",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 12,
-                          ),
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              const Text(
-                "Lets Get Started",
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey,
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
