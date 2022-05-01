9cbd397471c41105e02bba0ff9ff3c2727e40422
Dreamwalker <aristojeff@gmail.com>
2022-05-01 15:52:19 +0900

    Update office_meeting_app.dart

---
+++ lib/ep1139_office_meeting_app/office_meeting_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OfficeMeetingApp extends StatelessWidget {
+  const OfficeMeetingApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: OfficeMainPage(),
+    );
+  }
+}
