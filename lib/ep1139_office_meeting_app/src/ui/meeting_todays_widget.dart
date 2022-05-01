b5948a484d8b64c7e79ee791e1fcedcdbaefce1f
Dreamwalker <aristojeff@gmail.com>
2022-05-01 16:05:29 +0900

    Create meeting_todays_widget.dart

---
+++ lib/ep1139_office_meeting_app/src/ui/meeting_todays_widget.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class MeetingTodaysWidget extends StatefulWidget {
+  const MeetingTodaysWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MeetingTodaysWidget> createState() => _MeetingTodaysWidgetState();
+}
+
+class _MeetingTodaysWidgetState extends State<MeetingTodaysWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
