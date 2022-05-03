fe593f6ee3760497cb2877cce3259a0f5196629e
Dreamwalker <aristojeff@gmail.com>
2022-05-03 20:56:29 +0900

    Create meeting_button_widget.dart

---
+++ lib/ep1139_office_meeting_app/src/ui/widgets/meeting_button_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_notebook_19th_story/ep1139_office_meeting_app/src/enums/meeting_state.dart';
+
+class MeetingButtonWidget extends StatelessWidget {
+  MeetingButtonWidget({
+    Key? key,
+    this.enumMeetingState,
+    this.color,
+  }) : super(key: key);
+  EnumMeetingState? enumMeetingState;
+  MaterialColor? color;
+
+  @override
+  Widget build(BuildContext context) {
+    if (enumMeetingState == EnumMeetingState.online) {
+      return Container(
+        decoration: BoxDecoration(
+          color: color![700],
+          borderRadius: BorderRadius.circular(3),
+        ),
+        padding: const EdgeInsets.all(8),
+        child: Row(
+          children: const [
+            Text(
+              "Join Now",
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+            SizedBox(
+              width: 4,
+            ),
+            CircleAvatar(
+              radius: 8,
+              backgroundColor: Colors.black,
+              child: Icon(
+                Icons.keyboard_arrow_right,
+                color: Colors.white,
+                size: 12,
+              ),
+            )
+          ],
+        ),
+      );
+    }
+    return Container();
+  }
+}
