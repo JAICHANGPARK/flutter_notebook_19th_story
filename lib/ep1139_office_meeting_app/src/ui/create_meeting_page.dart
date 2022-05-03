547f3865666cac6985f99c49d06b0443dbfb2a80
Dreamwalker <aristojeff@gmail.com>
2022-05-03 21:15:58 +0900

    Update create_meeting_page.dart

--- lib/ep1139_office_meeting_app/src/ui/create_meeting_page.dart
+++ lib/ep1139_office_meeting_app/src/ui/create_meeting_page.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class CreateMeetingPage extends StatefulWidget {
@@ -13,7 +11,48 @@ class _CreateMeetingPageState extends State<CreateMeetingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 24),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 24,
+                      width: 24,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.blueAccent,
+                      ),
+                      child: const Icon(
+                        Icons.keyboard_arrow_left,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const Expanded(
+                      child: Center(
+                        child: Text(
+                          "Create Meeting",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 36,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
