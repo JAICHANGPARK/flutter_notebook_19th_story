a48b44b5d025cd80dc2d0ab326540d3b47945ece
Dreamwalker <aristojeff@gmail.com>
2022-05-03 20:58:00 +0900

    Update meeting_button_widget.dart

--- lib/ep1139_office_meeting_app/src/ui/widgets/meeting_button_widget.dart
+++ lib/ep1139_office_meeting_app/src/ui/widgets/meeting_button_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_19th_story/ep1139_office_meeting_app/src/enums/meeting_state.dart';
+import 'package:flutter_notebook_19th_story/generated/i18n.dart';
 
 class MeetingButtonWidget extends StatelessWidget {
   MeetingButtonWidget({
@@ -23,6 +24,37 @@ class MeetingButtonWidget extends StatelessWidget {
           children: const [
             Text(
               "Join Now",
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+            SizedBox(
+              width: 4,
+            ),
+            CircleAvatar(
+              radius: 8,
+              backgroundColor: Colors.black,
+              child: Icon(
+                Icons.keyboard_arrow_right,
+                color: Colors.white,
+                size: 12,
+              ),
+            )
+          ],
+        ),
+      );
+    }
+    else if(enumMeetingState == EnumMeetingState.notYet){
+      return Container(
+        decoration: BoxDecoration(
+          color: color![700],
+          borderRadius: BorderRadius.circular(3),
+        ),
+        padding: const EdgeInsets.all(8),
+        child: Row(
+          children: const [
+            Text(
+              "Notify Me",
               style: TextStyle(
                 fontSize: 12,
               ),
