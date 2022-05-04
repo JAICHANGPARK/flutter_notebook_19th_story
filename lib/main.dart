40b0726457930efd774ba741677606f8032470b5
Dreamwalker <aristojeff@gmail.com>
2022-05-05 08:00:31 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_19th_story/code_jam/bacterial/baterial_gen_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'ep1139_office_meeting_app/office_meeting_app.dart';
 
 main() {
   runApp(
     const ProviderScope(
-      child: OfficeMeetingApp(),
+      child: BaterialGenApp(),
     ),
   );
 }
