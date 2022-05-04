08dfef92c856f12b10c682bc5cb274ec42a5a943
Dreamwalker <aristojeff@gmail.com>
2022-05-05 08:00:27 +0900

    Update baterial_gen_app.dart

--- lib/code_jam/bacterial/baterial_gen_app.dart
+++ lib/code_jam/bacterial/baterial_gen_app.dart
@@ -2,11 +2,15 @@
 
 import 'package:flutter/material.dart';
 
+import 'src/ui/interative_widget.dart';
+
 class BaterialGenApp extends StatelessWidget {
   const BaterialGenApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: PetriDishIterative(),
+    );
   }
 }
