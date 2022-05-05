fd11a0c1431bd4c703f872142ff8c4261c9295f7
Dreamwalker <aristojeff@gmail.com>
2022-05-05 12:09:18 +0900

    Update fitness_start_screen.dart

--- lib/ep1143_fitness_mobile_app/src/fitness_start_screen.dart
+++ lib/ep1143_fitness_mobile_app/src/fitness_start_screen.dart
@@ -15,7 +15,10 @@ class FitnessStartScreen extends StatelessWidget {
             bottom: 0,
             right: 0,
             child: ColorFiltered(
-              colorFilter: ColorFilter.mode(Colors.blue.withOpacity(0.4), BlendMode.colorBurn),
+              colorFilter: ColorFilter.mode(
+                Colors.blue.withOpacity(0.4),
+                BlendMode.colorBurn,
+              ),
               child: CachedNetworkImage(
                 imageUrl: 'https://cdn.pixabay.com/photo/2016/11/19/16/48/woman-1840275_960_720.jpg',
                 fit: BoxFit.cover,
@@ -24,7 +27,7 @@ class FitnessStartScreen extends StatelessWidget {
           ),
           Positioned(
             left: 24,
-            bottom: 32,
+            bottom: 64,
             right: 72,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -33,10 +36,10 @@ class FitnessStartScreen extends StatelessWidget {
                   backgroundColor: Colors.white.withOpacity(0.5),
                   radius: 28,
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 24,
                 ),
-                Text(
+                const Text(
                   "Transform",
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
@@ -44,7 +47,7 @@ class FitnessStartScreen extends StatelessWidget {
                     color: Colors.white,
                   ),
                 ),
-                Text(
+                const Text(
                   "your life",
                   style: TextStyle(
                     fontWeight: FontWeight.w300,
@@ -52,7 +55,7 @@ class FitnessStartScreen extends StatelessWidget {
                     color: Colors.white,
                   ),
                 ),
-                Text(
+                const Text(
                   "for better",
                   style: TextStyle(
                     fontWeight: FontWeight.w300,
@@ -60,22 +63,28 @@ class FitnessStartScreen extends StatelessWidget {
                     color: Colors.white,
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 16,
                 ),
-                Text(
+                const Text(
                   "Don't waste time and start your new life today with a personalized training plan",
                   style: TextStyle(color: Colors.white),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 42,
                 ),
                 Container(
-                  decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(24)),
-                  padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 16,
+                  ),
                   child: Row(
                     mainAxisSize: MainAxisSize.min,
-                    children: [
+                    children: const [
                       Text("Start Now"),
                       SizedBox(
                         width: 32,
