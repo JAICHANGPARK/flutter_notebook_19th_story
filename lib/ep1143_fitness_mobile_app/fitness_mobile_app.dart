9bf3d27fbc2c6bd136f46397cebe1fa8c6c17313
Dreamwalker <aristojeff@gmail.com>
2022-05-05 12:03:07 +0900

    Update fitness_mobile_app.dart

--- lib/ep1143_fitness_mobile_app/fitness_mobile_app.dart
+++ lib/ep1143_fitness_mobile_app/fitness_mobile_app.dart
@@ -1,12 +1,14 @@
-
-
 import 'package:flutter/material.dart';
+
+import 'src/fitness_start_screen.dart';
 
 class FitnessMobileApp extends StatelessWidget {
   const FitnessMobileApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: FitnessStartScreen(),
+    );
   }
 }
