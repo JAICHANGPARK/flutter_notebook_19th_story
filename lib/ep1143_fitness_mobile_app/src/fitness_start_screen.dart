70a268cb64b5f056430a612e5e281698c109cde3
Dreamwalker <aristojeff@gmail.com>
2022-05-05 12:05:45 +0900

    Update fitness_start_screen.dart

--- lib/ep1143_fitness_mobile_app/src/fitness_start_screen.dart
+++ lib/ep1143_fitness_mobile_app/src/fitness_start_screen.dart
@@ -15,7 +15,7 @@ class FitnessStartScreen extends StatelessWidget {
             bottom: 0,
             right: 0,
             child: ColorFiltered(
-              colorFilter: ColorFilter.mode(Colors.blue.withOpacity(0.6), BlendMode.color),
+              colorFilter: ColorFilter.mode(Colors.blue.withOpacity(0.2), BlendMode.color),
               child: CachedNetworkImage(
                 imageUrl: 'https://cdn.pixabay.com/photo/2016/11/19/16/48/woman-1840275_960_720.jpg',
                 fit: BoxFit.cover,
@@ -25,10 +25,36 @@ class FitnessStartScreen extends StatelessWidget {
           Positioned(
             left: 24,
             bottom: 32,
-            right: 0,
+            right: 72,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [],
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.white.withOpacity(0.5),
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                Text(
+                  "Transform",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32,
+                    color: Colors.white,
+                  ),
+                ),
+                Text("your life",  style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 32,
+                  color: Colors.white,
+                ),),
+                Text("for better",  style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 32,
+                  color: Colors.white,
+                ),),
+                Text("Don't waste time and start your new life today with a personalized training plan"),
+              ],
             ),
           )
         ],
