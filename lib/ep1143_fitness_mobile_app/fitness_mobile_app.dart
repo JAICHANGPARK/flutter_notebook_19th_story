814e0c8817514bc37b93670fe8bd53e7906a24f8
Dreamwalker <aristojeff@gmail.com>
2022-05-05 11:57:37 +0900

    Create fitness_mobile_app.dart

---
+++ lib/ep1143_fitness_mobile_app/fitness_mobile_app.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class FitnessMobileApp extends StatelessWidget {
+  const FitnessMobileApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
