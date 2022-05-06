5fbdb7b59d36b7953a14aa1037fd81718276dcab
Dreamwalker <aristojeff@gmail.com>
2022-05-06 15:53:15 +0900

    Update fitness_mobile_main_page.dart

---
+++ lib/ep1143_fitness_mobile_app/src/ui/fitness_mobile_main_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class FitnessMobileMainPage extends StatefulWidget {
+  const FitnessMobileMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<FitnessMobileMainPage> createState() => _FitnessMobileMainPageState();
+}
+
+class _FitnessMobileMainPageState extends State<FitnessMobileMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+
+    );
+  }
+}
