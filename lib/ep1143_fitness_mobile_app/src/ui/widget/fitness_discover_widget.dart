0ac6bf121b59b23e850b768b18ff74def70a32bd
Dreamwalker <aristojeff@gmail.com>
2022-05-06 16:01:48 +0900

    Update fitness_discover_widget.dart

---
+++ lib/ep1143_fitness_mobile_app/src/ui/widget/fitness_discover_widget.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:flutter/material.dart';
+
+class FitnessDiscoverWidget extends StatelessWidget {
+  const FitnessDiscoverWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
