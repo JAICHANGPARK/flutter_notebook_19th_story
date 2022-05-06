1a86f4e134d89da5264404cd986f9c116d710378
Dreamwalker <aristojeff@gmail.com>
2022-05-06 16:04:56 +0900

    Update fitness_discover_widget.dart

--- lib/ep1143_fitness_mobile_app/src/ui/widget/fitness_discover_widget.dart
+++ lib/ep1143_fitness_mobile_app/src/ui/widget/fitness_discover_widget.dart
@@ -5,22 +5,43 @@ class FitnessDiscoverWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 160,
-          decoration: BoxDecoration(
-            color: Colors.blueGrey,
-            borderRadius: BorderRadius.circular(8),
+    return Padding(
+      padding: const EdgeInsets.only(top: 8, right: 0, left: 8),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8),
+            child: Container(
+              height: 160,
+              decoration: BoxDecoration(
+                color: Colors.blueGrey,
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
           ),
-        ),
-        Container(
-          height: 64,
-          decoration: BoxDecoration(
-            color: Colors.pink,
+          SizedBox(
+            height: 16,
           ),
-        )
-      ],
+          Container(
+            height: 64,
+            decoration: BoxDecoration(
+              color: Colors.pink,
+            ),
+          ),
+          Text(
+            "Exercises",
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 8),
+            child: Container(
+              height: 200,
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
