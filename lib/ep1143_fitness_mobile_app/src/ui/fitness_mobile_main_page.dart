1a20c82cdc7ec7bfbed4db525397f76dc5034dc3
Dreamwalker <aristojeff@gmail.com>
2022-05-06 15:57:14 +0900

    Update fitness_mobile_main_page.dart

--- lib/ep1143_fitness_mobile_app/src/ui/fitness_mobile_main_page.dart
+++ lib/ep1143_fitness_mobile_app/src/ui/fitness_mobile_main_page.dart
@@ -12,11 +12,39 @@ class _FitnessMobileMainPageState extends State<FitnessMobileMainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [
-
+        children: [],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+              ),
+              label: ""),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.local_fire_department,
+              ),
+              label: ""),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.restaurant,
+              ),
+              label: ""),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.workspace_premium,
+              ),
+              label: ""),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.settings,
+              ),
+              label: "")
         ],
       ),
-
     );
   }
 }
