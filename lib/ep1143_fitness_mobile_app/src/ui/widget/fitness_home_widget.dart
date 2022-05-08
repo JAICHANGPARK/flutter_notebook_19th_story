33edd2f5f59437d3a23c07fda5a5b1f439735a6f
Dreamwalker <aristojeff@gmail.com>
2022-05-08 13:54:38 +0900

    Create fitness_home_widget.dart

---
+++ lib/ep1143_fitness_mobile_app/src/ui/widget/fitness_home_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FitnessHomeWidget extends StatefulWidget {
+  const FitnessHomeWidget({Key? key}) : super(key: key);
+
+  @override
+  State<FitnessHomeWidget> createState() => _FitnessHomeWidgetState();
+}
+
+class _FitnessHomeWidgetState extends State<FitnessHomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
