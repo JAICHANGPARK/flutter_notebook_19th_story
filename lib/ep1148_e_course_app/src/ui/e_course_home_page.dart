0704152834421d4d7c7cdcad780031b803fa51d1
Dreamwalker <aristojeff@gmail.com>
2022-05-10 20:31:27 +0900

    Update e_course_home_page.dart

--- lib/ep1148_e_course_app/src/ui/e_course_home_page.dart
+++ lib/ep1148_e_course_app/src/ui/e_course_home_page.dart
@@ -13,50 +13,67 @@ class _ECourseHomePageState extends State<ECourseHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.brown[50],
-      body: Column(
-        children: [
-          Expanded(child: Placeholder()),
-          Divider(
-            color: Colors.grey,
-          ),
-          Container(
-            height: 72,
-            decoration: BoxDecoration(),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Ionicons.home_outline,
-                  ),
-                  iconSize: 28,
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Ionicons.list_circle_outline,
-                  ),
-                  iconSize: 28,
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Ionicons.bag_handle_outline,
-                  ),
-                  iconSize: 28,
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Ionicons.person_outline,
-                  ),
-                  iconSize: 28,
-                )
-              ],
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Temukan Kursus"),
+                  Text("Online Sesuai Minat"),
+                  Container(
+                    height: 240,
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                    ),
+                  )
+                ],
+              ),
             ),
-          )
-        ],
+            Divider(
+              color: Colors.grey,
+            ),
+            Container(
+              height: 72,
+              decoration: BoxDecoration(),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Ionicons.home_outline,
+                    ),
+                    iconSize: 28,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Ionicons.list_circle_outline,
+                    ),
+                    iconSize: 28,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Ionicons.bag_handle_outline,
+                    ),
+                    iconSize: 28,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Ionicons.person_outline,
+                    ),
+                    iconSize: 28,
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
