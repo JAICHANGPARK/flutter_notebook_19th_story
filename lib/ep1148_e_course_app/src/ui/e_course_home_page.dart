3e34dc752945f4ae63dfc70f7cd1857af0f223ec
Dreamwalker <aristojeff@gmail.com>
2022-05-10 20:15:42 +0900

    Create e_course_home_page.dart

---
+++ lib/ep1148_e_course_app/src/ui/e_course_home_page.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class ECourseHomePage extends StatefulWidget {
+  const ECourseHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<ECourseHomePage> createState() => _ECourseHomePageState();
+}
+
+class _ECourseHomePageState extends State<ECourseHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
