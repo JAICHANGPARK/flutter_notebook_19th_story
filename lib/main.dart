0da39196ad378028b69730fa46ca20b330762d23
Dreamwalker <aristojeff@gmail.com>
2022-05-10 20:17:10 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_19th_story/ep1143_fitness_mobile_app/fitness_mobile_app.dart';
+import 'package:flutter_notebook_19th_story/ep1148_e_course_app/e_course_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 main() {
   runApp(
     const ProviderScope(
-      child: FitnessMobileApp(),
+      child: ECourseApp(),
     ),
   );
 }
