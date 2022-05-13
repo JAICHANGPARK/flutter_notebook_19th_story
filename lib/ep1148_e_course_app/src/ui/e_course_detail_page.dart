8b1ef63194fa7efb13ad664f7f537c02d983feb2
Dreamwalker <aristojeff@gmail.com>
2022-05-13 20:25:15 +0900

    Update e_course_detail_page.dart

---
+++ lib/ep1148_e_course_app/src/ui/e_course_detail_page.dart
@@ -0,0 +1,25 @@
+
+
+import 'package:flutter/material.dart';
+
+class EcourseDetailPage extends StatefulWidget {
+  const EcourseDetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<EcourseDetailPage> createState() => _EcourseDetailPageState();
+}
+
+class _EcourseDetailPageState extends State<EcourseDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+          )
+        ],
+      ),
+    );
+  }
+}
